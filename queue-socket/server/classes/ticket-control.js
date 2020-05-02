788849086cc8c17ef3d5f4709d65ccb60987f611
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-01 22:07:33 -0500

    [Update] Created method to attend ticket with a new array for the last four

--- queue-socket/server/classes/ticket-control.js
+++ queue-socket/server/classes/ticket-control.js
@@ -12,12 +12,14 @@ class TicketControl {
     this.ultimo = 0;
     this.hoy = new Date().getDate();
     this.tickets = [];
+    this.ultimos4 = [];
 
     let data = JSON.parse(fs.readFileSync('./server/data/data.json'));
 
     if (data.hoy === this.hoy) {
       this.ultimo = data.ultimo;
       this.tickets = data.tickets;
+      this.ultimos4 = data.ultimos4;
     } else {
       this.reiniciarConteo();
     }
@@ -40,9 +42,34 @@ class TicketControl {
     return `Ticket ${this.ultimo}`;
   }
 
+  atenderTicket(escritorio) {
+    if (this.tickets.length === 0) {
+      return 'No hay tickets';
+    }
+
+    let numeroTicket = this.tickets[0].numero;
+    this.tickets.shift();
+
+    let ticketPorAtender = new Ticket(numeroTicket, escritorio);
+
+    this.ultimos4.unshift(ticketPorAtender);
+
+    if (this.ultimos4.lengt > 4) {
+      this.ultimos4.pop();
+    }
+
+    console.log('Ultimos 4');
+    console.log(this.ultimos4);
+
+    this.grabarArchivo();
+
+    return this.ticketPorAtender;
+  }
+
   reiniciarConteo() {
     this.ultimo = 0;
     this.tickets = [];
+    this.ultimos4 = [];
 
     console.log('Se ha inicializado el sistema');
 
@@ -54,6 +81,7 @@ class TicketControl {
       ultimo: this.ultimo,
       hoy: this.hoy,
       tickets: this.tickets,
+      ultimos4: this.ultimos4,
     };
 
     let jsonDataString = JSON.stringify(jsonData);
